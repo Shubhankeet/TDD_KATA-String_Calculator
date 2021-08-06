6b93b892ff18f55a6372bb4e686925249b61ff46
senap <senap@MSI>
2021-08-06 09:02:05 +0530

    refactoring the code where a condition will check if the given input starts with "//" or else new line; input String is calculated accordingly

--- TDD KATA - String Calculator/src/shubhankeet/kata_stringCalculator/StringCalculator.java
+++ TDD KATA - String Calculator/src/shubhankeet/kata_stringCalculator/StringCalculator.java
@@ -5,17 +5,27 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
 	
+	private String delimiter;
+	private String numberString;
+	
+	private StringCalculator(String delimiter, String numberString) {
+		this.delimiter = delimiter;
+		this.numberString = numberString;
+	}
+	
 	public static int Add(String numbers) {
 		if (numbers.isEmpty())
 			return 0;
 		
-		String delimiterString = ",|\n";
+		StringCalculator calculateInput;
 		if (numbers.startsWith("//")) {
 			String []items = numbers.split("\n", 2);
-			delimiterString = items[0].substring(2);
-			numbers = items[1];
+			calculateInput = new StringCalculator(items[0].substring(2), items[1]);
+		} else {
+			calculateInput = new StringCalculator(",|\n", numbers);
 		}
-			Stream<String> numberArray = Arrays.stream(numbers.split(delimiterString));
+		
+			Stream<String> numberArray = Arrays.stream(calculateInput.numberString.split(calculateInput.delimiter));
 			return numberArray.mapToInt(Integer::parseInt).sum();
 		}
 			
