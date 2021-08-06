5ba3e71fe916a3d4c6d6853ceca133f3522e6a89
senap <senap@MSI>
2021-08-06 09:18:21 +0530

    refactoring by removing call by refernce to the instance of the methoStringCalculator without using Stream. Also, reformatting the code.

--- TDD KATA - String Calculator/src/shubhankeet/kata_stringCalculator/StringCalculator.java
+++ TDD KATA - String Calculator/src/shubhankeet/kata_stringCalculator/StringCalculator.java
@@ -1,34 +1,36 @@
 package shubhankeet.kata_stringCalculator;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class StringCalculator {
-	
+
 	private String delimiter;
 	private String numberString;
-	
+
 	private StringCalculator(String delimiter, String numberString) {
 		this.delimiter = delimiter;
 		this.numberString = numberString;
 	}
-	
+
+	private int sumOfStrings() {
+		return Arrays.stream(numberString.split(delimiter)).mapToInt(Integer::parseInt).sum();
+	}
+
 	public static int Add(String numbers) {
 		if (numbers.isEmpty())
 			return 0;
-		
+
 		StringCalculator calculateInput = parseTheInputString(numbers);
-		
-			Stream<String> numberArray = Arrays.stream(calculateInput.numberString.split(calculateInput.delimiter));
-			return numberArray.mapToInt(Integer::parseInt).sum();
-		}
+
+		return calculateInput.sumOfStrings();
+	}
 
 	private static StringCalculator parseTheInputString(String numbers) {
 		if (numbers.startsWith("//")) {
-			String []items = numbers.split("\n", 2);
+			String[] items = numbers.split("\n", 2);
 			return new StringCalculator(items[0].substring(2), items[1]);
 		} else {
 			return new StringCalculator(",|\n", numbers);
 		}
-	}		
+	}
 }
